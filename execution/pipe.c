d54049950665dbc9a67cf1c871064f13b004d004
Said Echlahbi <sechlahb@c2r1p3.1337.ma>
2025-06-24 15:11:07 +0100

    lhbal

--- execution/pipe.c
+++ execution/pipe.c
@@ -1,77 +1,131 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   pipe.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sechlahb <sechlahb@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/19 16:43:52 by sechlahb          #+#    #+#             */
-/*   Updated: 2025/06/19 18:40:09 by sechlahb         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   pipe.c                                             :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: sechlahb <sechlahb@student.42.fr>          +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2025/06/19 16:43:52 by sechlahb          #+#    #+#             */
+// /*   Updated: 2025/06/21 17:13:31 by sechlahb         ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+// #include "../includes/minishell.h"
 
-char* get_right_path(t_env *env, char *cmd)
-{
-    char **paths;
-
-    paths = NULL;
-    if (!access(cmd, X_OK))
-        return cmd;
-    while (env)
-    {
-        if (ft_strcmp(env->key , "PATH") == 0)
-        {
-            paths = ft_split(env->value, ':');
-            break;
-        }
-        env = env->next;
-    }
-    char *str;
-    if (paths)
-    {
-        while (*(paths++))
-        {
-            str = ft_strjoin(*paths, cmd);
-            if (!access(str, X_OK))
-                return str;
-            free(str);
-        }
-    }
-    return NULL;
-}
-
-void first_pipe(t_fds fds, )
-{
-    char *str = "ls"
-
-
-
-    char *str = "/bin/ls";
-    char **ss = {"ls", "-la", NULL};
-    execve(str, ss, NULL)
-}
-void middle_pipe(t_fds *fds)
-{
+// static char **get_paths(t_env *env)
+// {
+//     char **paths;
     
-}
-void last_pipe(t_fds fds)
-{
-    
-}
+//     paths = NULL;
+//     while (env)
+//     {
+//         if (ft_strcmp(env->key , "PATH") == 0)
+//         {
+//             paths = ft_split(env->value, ':');
+//             if (!paths)
+//                 return NULL;
+//             return paths;
+//         }
+//         env = env->next;
+//     }
+//     return NULL;
+// }
 
-void pipes(t_token *token, t_env *env)
-{
-    t_fds fds;
-    
-    int i = 2;
-    char *cmd = get_right_path(env, );
-    first_pipe()
-    while (i < 2)
-    {
+// static char *get_right_path(t_env *env, char *cmd)
+// {
+//     char **paths;
+//     char *cmd_with_path, *str;
+
+//     if (!access(cmd, X_OK))
+//         return cmd;
+//     paths = get_paths(env);
+//     if (!paths)
+//         return NULL;
+//     while (*paths)
+//     {
+//         str = ft_strjoin(*paths, "/");
+//         cmd_with_path = ft_strjoin(str, cmd);
+//         free(str);
+//         if (access(cmd_with_path, X_OK) == 0)
+//             return (cmd_with_path);
+//         free(cmd_with_path);
+//         paths++;
+//     }
+//     return NULL;
+// }
+
+// void first_pipe(t_fds fds)
+// {
+//     int pid = fork();
+//     if (pid == 0)
+//     { 
+//         dup2(fds.pipefd1[1], 1);
+//         close(fds.pipefd1[1]);
+//         close(fds.pipefd1[0]);
+//         close(fds.pipefd2[1]);
+//         close(fds.pipefd2[0]);
+//         execve(str, ss, NULL);
+//     }
+//     return;
+// }
+
+// void middle_pipe(t_fds fds)
+// {
+//     int pid = fork();
+//     if (pid == 0)
+//     {
+//         dup2(fds.pipefd1[0], 0);
+//         close(fds.pipefd1[1]);
+//         close(fds.pipefd1[0]);
+//         dup2(fds.pipefd2[1], 1);
+//         close(fds.pipefd2[1]);
+//         close(fds.pipefd2[0]);
+//         execve(str, ss, NULL);
+//     }
+// }
+
+// void last_pipe(t_fds fds)
+// {
+//     int pid = fork();
+//     if (pid == 0)
+//     {
+//         dup2(fds.pipefd1[0], 0);
+//         close(fds.pipefd1[1]);
+//         close(fds.pipefd1[0]);
         
-    }
-    last_pipe()
+
+//         close(fds.pipefd2[1]);
+//         close(fds.pipefd2[0]);
+        
+//         char *str = "/bin/wc";
+//         char *ss[] = {"wc", "-l", NULL};
+//         execve(str, ss, NULL);
+//     }
+// }
+
+// char **get_(t_token *token)
+// {
+//     char ***pipes;
+//     int start;
+
+//     start = 0;
+//     while (token)
+//     {
+//         if (!ft_strcmp(token->value, "<"))
+//     }
+// }
+
+// void pipes(t_token *token, t_env *env)
+// {
+//     t_fds fds;
+
+//     int i = 2;
+//     char *cmd = get_right_path(env, );
+//     first_pipe()
+//     while (i < 2)
+//     {
+        
+//     }
+//     last_pipe()
     
-}
+// }
