f4a92864c3166015d78cb717252c787c5ecce9e6
Said Chahir <schahir@c1r9p3.1337.ma>
2025-07-24 12:10:56 +0100

    shell

--- parsing/split_n_insert.c
+++ parsing/split_n_insert.c
@@ -6,7 +6,7 @@
 /*   By: schahir <schahir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 13:32:35 by schahir           #+#    #+#             */
-/*   Updated: 2025/07/24 11:26:49 by schahir          ###   ########.fr       */
+/*   Updated: 2025/07/24 11:55:28 by schahir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,84 +19,83 @@ static void	skip_spaces(t_scanner *var, char *input)
 	var->start = var->i;
 }
 
-static int	to_split(t_scanner *var, char *input, char *encapsulizer)
+static void	to_split(t_scanner *var, char *input, char *encapsulizer)
 {
+	// Add null check for input
+	if (!input[var->i])
+		return;
+		
 	if (!ft_strncmp(&input[var->i], encapsulizer, 19))
 	{
 		var->i += 19;
 		var->encapsuled = !var->encapsuled;
-		return (1);
 	}
 	else if (input[var->i] == '\'' && !var->in_dquote && !var->encapsuled)
 	{
 		var->in_squote = !var->in_squote;
-		var->i++;
-		return (1);
 	}
 	else if (input[var->i] == '"' && !var->in_squote && !var->encapsuled)
 	{
 		var->in_dquote = !var->in_dquote;
-		var->i++;
-		return (1);
 	}
-	return (0);
 }
 
-static int	count_tok(char *input, char *encapsulizer)
+int	count_tok(char *input, char *encapsulizer)
 {
 	int			count;
 	t_scanner	var;
-
+	
+	if (!input)
+		return (0);
+		
 	ft_bzero(&var, sizeof(var));
 	count = 0;
 	while (input[var.i])
 	{
-		if (!to_split(&var, input, encapsulizer))
+		to_split(&var, input, encapsulizer);
+		if (!input[var.i])
+			break;
+		if (!var.in_squote && !var.in_dquote && ft_isspace(input[var.i]))
 		{
-			if (!var.in_squote && !var.in_dquote && ft_isspace(input[var.i]))
-			{
-				if (var.i > var.start)
-					count++;
-				skip_spaces(&var, input);
-			}
-			else
-				var.i++;
+			if (var.i > var.start)
+				count++;
+			skip_spaces(&var, input);
 		}
+		else
+			var.i++;
 	}
 	if (var.i > var.start)
 		count++;
 	return (count);
 }
 
-static char	**str_tok(char *input, char *encapsulizer, t_garbage **garbage)
+char	**str_tok(char *input, char *enc, t_garbage **garbage)
 {
 	char		**tokens;
 	int			j;
 	t_scanner	v;
-
+	
 	ft_bzero(&v, sizeof(v));
 	tokens = NULL;
 	j = 0;
-	tokens = ft_malloc(sizeof(char *), (count_tok(input, encapsulizer) + 1),
-			garbage);
+	tokens = ft_malloc(sizeof(char *), (count_tok(input, enc) + 1), garbage);
 	while (input[v.i])
 	{
-		if (!to_split(&v, input, encapsulizer))
+		to_split(&v, input, enc);
+		if (!input[v.i])
+			break;
+		if (!v.in_squote && !v.in_dquote && ft_isspace(input[v.i]))
 		{
-			if (!v.in_squote && !v.in_dquote && ft_isspace(input[v.i]))
-			{
-				if (v.i > v.start)
-					tokens[j++] = _substr(input, v.start, v.i - v.start, garbage);
-				skip_spaces(&v, input);
-			}
-			else
-				v.i++;
+			if (v.i > v.start)
+				tokens[j++] = _substr(input, v.start, v.i - v.start, garbage);
+			skip_spaces(&v, input);
 		}
+		else
+			v.i++;
 	}
 	if (v.i > v.start)
 		tokens[j++] = _substr(input, v.start, v.i - v.start, garbage);
-	tokens[j] = NULL;
-	return (tokens);
+	return (tokens[j] = NULL, tokens);
 }
 
 void	split_n_insert(t_token *cur, char *enapsulizer, t_garbage **garbage)
@@ -104,10 +103,12 @@ void	split_n_insert(t_token *cur, char *enapsulizer, t_garbage **garbage)
 	char	**splits;
 	t_token	*new;
 	int		i;
-
-	if (!cur->value[0])
+	
+	if (!cur || !cur->value || !cur->value[0])
 		return ;
 	splits = str_tok(cur->value, enapsulizer, garbage);
+	if (!splits || !splits[0])
+		return ;
 	cur->value = ft_strdup(splits[0], garbage);
 	cur->type = ARG;
 	cur->is_ambg = AMBIGIUOS;
